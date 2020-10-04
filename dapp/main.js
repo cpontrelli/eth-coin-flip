674d8da53cd161fc0de28c866591b9e74868a95c
Charlie Pontrelli <cpontrelli12@gmail.com>
2020-10-03 18:36:17 -0700

    Refactored alert messages

--- dapp/main.js
+++ dapp/main.js
@@ -2,20 +2,43 @@ var web3 = new Web3(Web3.givenProvider);
 var address = "0x116b8E810E255CA3A099d20ef6CcB06A41ea7219";
 var contractInstance;
 
+var alert = `<div class="alert alert-dismissible fade show" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>`
 
 $(document).ready(function() {
     window.ethereum.enable().then(function(accounts) {
         contractInstance = new web3.eth.Contract(abi, address, {from: accounts[0]});
-        latestBlock = web3.eth.blockNumber
         
         console.log(contractInstance);
 
         contractInstance.events.allEvents()
             .on('data', function(event){
                 if(event.event == "flipWon") {
-                    alert("Congratulations!");
+                    let winningAlert = $.parseHTML(alert);
+                    $(winningAlert).addClass("alert-success");
+                    $(winningAlert).prepend("<strong>Flip Won!</strong> Your winnings have been transferred.");
+                    $("#bet-alerts").prepend(winningAlert);
+                    setTimeout(() => $(winningAlert).alert('close'), 5000);
                 } else if (event.event == "flipLost") {
-                    alert("You're a Loser!");
+                    let losingAlert = $.parseHTML(alert);
+                    $(losingAlert).addClass("alert-danger");
+                    $(losingAlert).prepend("<strong>Flip Lost</strong> Thanks for playing!");
+                    $("#bet-alerts").prepend(losingAlert);
+                    setTimeout(() => $(losingAlert).alert('close'), 5000);
+                } else if (event.event == "coinFlipped") {
+                    let flippedAlert = $.parseHTML(alert);
+                    $(flippedAlert).addClass("alert-primary");
+                    if(event.returnValues.result == "0")
+                    {
+                        $(flippedAlert).prepend("<strong>Heads</strong>");
+                    } else {
+                        $(flippedAlert).prepend("<strong>Tails</strong>");
+                    }
+                    $("#bet-alerts").prepend(flippedAlert);
+                    setTimeout(() => $(flippedAlert).alert('close'), 5000);
                 }
             });
     });
@@ -34,7 +57,11 @@ async function placeBet(){
     if(balance >= bet && bet > 0) {
         contractInstance.methods.settleBet(prediction).send({value: bet})
     } else {
-        alert("Bet must be greater then 0 and less than " + balance/(10**18) + " ETH." );
+        let warning = $.parseHTML(alert);
+        $(warning).addClass("alert-danger");
+        $(warning).prepend("Bet must be <strong>greater</strong> than 0 and <strong>less</strong> than " + balance/(10**18) + " ETH.");
+        $("#bet-alerts").prepend(warning);
+        setTimeout(() => $(warning).alert('close'), 5000);
     }
 }
 
